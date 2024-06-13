5dc7b98742ea43f799a26e8e3736802a97406693
fifartdev <fifartapp@gmail.com>
2024-06-13 22:33:31 +0300

    referees page loading indicator implemented

--- app/referees/loading.jsx
+++ app/referees/loading.jsx
@@ -2,14 +2,8 @@ export default function loadingRefereesPage(){
     return (
         <main className="flex justify-center items-center min-h-screen">
        <div className="w-full max-w-lg p-4">
-  <div className="animate-pulse">
-    <h1 className="text-gray-700 font-bold text-xl mb-4">Περιμένετε όσο φορτώνει...</h1>
-    <div className="bg-gray-300 h-4 w-32 rounded-full mb-2"></div>
-    <div className="bg-gray-300 h-4 w-48 rounded-full mb-2"></div>
-    <div className="bg-gray-300 h-4 w-24 rounded-full mb-2"></div>
-    <div className="bg-gray-300 h-4 w-64 rounded-full mb-2"></div>
-    <div className="bg-gray-300 h-4 w-40 rounded-full mb-2"></div>
-  </div>
+       <div class="w-12 h-12 border-8 border-blue-500 border-solid border-t-transparent rounded-full animate-spin"></div>
+
 </div>
 </main>
     )
--- app/referees/page.jsx
+++ app/referees/page.jsx
@@ -8,7 +8,7 @@ import Link from 'next/link'
         const [acat,setAcat] = useState([])
         const [bcat,setBcat] = useState([])
         const [trial,setTrial] = useState([])
-        
+        const [isLoading,setisLoading] = useState(false)
         ////////Ιnactive Queries
         const [interInact,setInterInact] = useState([])
         const [acatInact,setAcatInact] = useState([])
@@ -18,6 +18,7 @@ import Link from 'next/link'
         
 
         const getRefs = async ()=> {
+            setisLoading(true)
             try {
             
             const intRes = await db.listDocuments(ODKE_DB, COL_REFS, [
@@ -64,7 +65,7 @@ import Link from 'next/link'
             setInterInact(intInactRes.documents)
             setAcatInact(acatInactRes.documents)
             setBcatInact(bcatInactRes.documents)
-               
+            setisLoading(false)
         } catch (error) {
                 console.log('Error from referees page: ', error.message);
             }
@@ -77,7 +78,31 @@ import Link from 'next/link'
         // console.log('A Category are: ', acat);
         // console.log('B Category are: ', bcat);
         // console.log('Trials are: ', trial);
-  
+    
+        if(isLoading){
+            return (
+                <main className="flex justify-center min-h-screen">
+       <div className="w-full p-10">
+       <div className='flex justify-center bg-cyan-500 p-5'>
+        <img src="https://oseka.gr/wp-content/uploads/2018/11/logo-oseka-white.png"/>
+      </div>
+  <nav className="flex items-center justify-between flex-wrap bg-blue-800 p-6">
+      <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <Link href="/">
+          <span className="font-semibold text-xl tracking-tight cursor-pointer m-3">Αρχική</span>
+        </Link>|
+        <Link href="/referees">
+          <span className="font-semibold text-md tracking-tight cursor-pointer m-3">Διαιτητές</span>
+        </Link>
+      </div>
+      </nav>
+      <div class="w-12 h-12 border-8 border-blue-500 border-solid border-t-transparent rounded-full animate-spin mt-5 ml-5"></div>
+      </div>
+      </main>
+            )
+        }
+
+
   
     return (
 
@@ -96,7 +121,7 @@ import Link from 'next/link'
         </Link>
       </div>
       </nav>
-            <h1 className="text-lg font-bold mb-2 mt-5">ΔΙΕΘΝΕΙΣ</h1>
+          { inter.length > 0 && <h1 className="text-lg font-bold mb-2 mt-5">ΔΙΕΘΝΕΙΣ</h1> }
         <ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 mt-4" >
             { inter?.map((r, i=0)=>{
                 return (
@@ -104,7 +129,7 @@ import Link from 'next/link'
                 )
             }) }
         </ul>
-        <h1 className="text-lg font-bold mb-2 mt-5">Α ΚΑΤΗΓΟΡΙΑ</h1>
+        { inter.length > 0 && <h1 className="text-lg font-bold mb-2 mt-5">Α ΚΑΤΗΓΟΡΙΑ</h1> }
         <ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 mt-4" >
             { acat?.map((r, i=0)=>{
                 return (
@@ -112,7 +137,7 @@ import Link from 'next/link'
                 )
             }) }
         </ul>
-        <h1 className="text-lg font-bold mb-2 mt-5">Β ΚΑΤΗΓΟΡΙΑ</h1>
+       {inter.length > 0 && <h1 className="text-lg font-bold mb-2 mt-5">Β ΚΑΤΗΓΟΡΙΑ</h1> }
         <ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 mt-4" >
             { bcat?.map((r, i=0)=>{
                 return (
@@ -120,7 +145,7 @@ import Link from 'next/link'
                 )
             }) }
         </ul>
-        <h1 className="text-lg font-bold mb- mt-5">ΔΟΚΙΜΟΙ</h1>
+       {inter.length > 0 && <h1 className="text-lg font-bold mb- mt-5">ΔΟΚΙΜΟΙ</h1> }
         <ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 mt-4" >
             { trial?.map((r, i=0)=>{
                 return (
@@ -128,7 +153,7 @@ import Link from 'next/link'
                 )
             }) }
         </ul>
-        <h1 className="text-2xl font-bold mb-2 mt-5">ΛΙΣΤΑ ΑΝΕΝΕΡΓΩΝ ΔΙΑΙΤΗΤΩΝ</h1>
+      {acatInact.length > 0 &&  <h1 className="text-2xl font-bold mb-2 mt-5">ΛΙΣΤΑ ΑΝΕΝΕΡΓΩΝ ΔΙΑΙΤΗΤΩΝ</h1> }
        { interInact.length > 0 && <h1 className="text-lg font-bold mb-2 mt-5">ΔΙΕΘΝΕΙΣ</h1> }
         <ul className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4 mt-4" >
             { interInact?.map((r, i=0)=>{
