19bde3c9fd5996ca54289d9d62bed64193267f2d
fifartdev <fifartapp@gmail.com>
2025-04-30 12:51:38 +0300

    Participation Counter updated with loading state

--- app/components/ParticipationCounter.jsx
+++ app/components/ParticipationCounter.jsx
@@ -4,43 +4,43 @@ import { ODKE_DB, db, COL_MATCHES, Query, COL_REFS } from '@/app/utils/appwrite'
 
 function ParticipationCounter(id) {
 
-        console.log('id is: ', id.id);
+        //console.log('id is: ', id.id);
         const [mdays, setMdays] = useState([])
         const [ref, setRef] = useState(null)
         const [loading, setLoading] = useState(false)
         const [refId, setRefId] = useState(id.id)
-        console.log('Ref ID is: ', refId);
+        //console.log('Ref ID is: ', refId);
         
         const getRefMatches = async ()=> {
-            
+          setLoading(true)
           try {
             const resp = await db.getDocument(ODKE_DB, COL_REFS, refId)
             setRef(resp)
             if(resp){
               const res = await db.listDocuments(ODKE_DB, COL_MATCHES, [Query.contains('referees', [resp.name]), Query.orderAsc('fulldate')])
               const final = res.documents
-              console.log('Data are', final);
-              setMdays(final)     
+              //console.log('Data are', final);
+              setMdays(final)
             }
             
           } catch (error) {
             console.log('Error from Participation Counter Component: ',error.message);
+          } finally {
+            setLoading(false)
           }
-        }
-    
+        } 
         
     
         useEffect(()=>{
-          setLoading(true)
+          
           getRefMatches()
-          setLoading(false)
+          
         },[])
-        console.log('Ref is', ref);
         
 
     return (
         <>
-       - <strong>Σύνολο Συμμετοχών:</strong> { loading ? 'Γίνεται καταμέτρηση' : mdays.length}
+       {loading ? <div class="w-2 h-2 border-8 border-blue-500 border-solid border-t-transparent rounded-full animate-spin"></div> : <>- <strong>Σύνολο Συμμετοχών: </strong> {mdays.length}</>}
         </>
             
     );
